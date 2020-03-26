c4bca9946c1ad9759f6c44a720a22bd6b10337de
Drew Stratford <drewstratford@outlook.com>
2020-03-26 19:31:17 +1300

    Thread: add blocking system.
    We introduce the Blocker class and use it to mantain lists
    of blocking threads. This replaces the old method which used
    a list of Threads.0

--- kernel/Thread.cpp
+++ kernel/Thread.cpp
@@ -2,8 +2,8 @@
 
 #include <devices/Serial.h>
 
-List<Thread> runnable_threads;
-List<Thread> dying_threads;
+List<Blocker> runnable_threads;
+List<Blocker> dying_threads;
 Thread kernel_thread;
 Thread *current_thread = &kernel_thread;
 
@@ -22,19 +22,36 @@ void Thread::switch_thread(Thread& from, Thread& to){
         : "memory");
 }
 
-Thread::Thread(){
-	reschedule();
+static void asm_dummy(){
+	// We need to pop the blocker and then return to
+	// the intended starting ip.
+	asm("kernel_thread_trampoline:\n"
+		"	add %0, %%esp\n"
+		"	ret\n"
+		:: "i"(sizeof(Blocker))
+		);
 }
+extern "C" void kernel_thread_trampoline(void);
+
+Thread::Thread() {}
 
 Thread::Thread(uintptr_t stack, uintptr_t start) {
 	stack_ptr = stack;
-	resume_ptr = start;
-	reschedule();
+	push_on_stack<uintptr_t>(start);
+	push_on_stack<Blocker>(Blocker(this));
+
+	Blocker* blocker = (Blocker*)stack_ptr;
+	resume_ptr = (uintptr_t)kernel_thread_trampoline;
+
+	set_state(ThreadState::Runnable);
+	runnable_threads.insert(blocker);
 }
 
-void Thread::reschedule(){
+
+void Thread::wait_for_cpu(){
+	Blocker cpu_blocker(this);
 	set_state(ThreadState::Runnable);
-	runnable_threads.insert_end(this);
+	runnable_threads.insert_end(&cpu_blocker);
 }
 
 void Thread::yield(){
@@ -46,26 +63,26 @@ void Thread::yield(){
 
 	com1().write_string("about to switch\n");
 	Thread *old_thread = current_thread;
-	current_thread = runnable_threads.pop();
+	current_thread = runnable_threads.pop()->get_thread();
 	if(current_thread == old_thread)
 		return;
 
 	if(old_thread->get_state() == ThreadState::Running)
-		old_thread->reschedule();
+		old_thread->wait_for_cpu();
 
 	current_thread->set_state(ThreadState::Running);
 	switch_thread(*old_thread, *current_thread);
 }
 
 void Thread::die(){
+	Blocker death_blocker(current_thread);
 	current_thread->set_state(ThreadState::Dead);
-	dying_threads.insert_end(current_thread);
+	dying_threads.insert_end(&death_blocker);
 	yield();
 }
 
 void Thread::initialize(){
 	//We set up the kernel thread to be the current running thread.
-	runnable_threads.remove(&kernel_thread);
 	current_thread = &kernel_thread;
 	current_thread->set_state(ThreadState::Running);
 }
--- kernel/Thread.h
+++ kernel/Thread.h
@@ -15,12 +15,19 @@ class Thread : public ListNode<Thread>{
 public:
 	Thread();
 	Thread(uintptr_t stack, uintptr_t resume);
+
+	template <typename T>
+	void push_on_stack(T t){
+		stack_ptr -= sizeof(T);
+		*(T*)stack_ptr = t;
+	};
+
 	static void switch_thread(Thread& from, Thread& to);
 	static void yield();
 	static void die();
 	static void initialize();
 
-	void reschedule();
+	void wait_for_cpu();
 	ThreadState get_state() const { return state; };
 	void set_state(ThreadState new_state) { state = new_state; };
 
@@ -30,3 +37,20 @@ private:
 	ThreadState state;
 };
 
+enum BlockerStatus
+	{ Ok
+	, Waiting
+	, Failure
+	, Signalled
+	};
+
+class Blocker : public ListNode<Blocker>{
+public:
+	Blocker(Thread *thread){ this->thread = thread; }
+	void set_status(BlockerStatus new_status) { status = new_status; };
+	BlockerStatus get_status() { return status; };
+	Thread* get_thread(){ return thread; };
+protected:
+	Thread *thread;
+	BlockerStatus status { Waiting };
+};
