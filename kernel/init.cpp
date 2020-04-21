fa448b936d4a1f467084ad0a00e4f60c9378c872
Drew Stratford <drewstratford@outlook.com>
2020-04-21 16:18:04 +1200

    Kernel: update init.cpp

--- kernel/init.cpp
+++ kernel/init.cpp
@@ -1,28 +1,35 @@
 #include <icxxabi.h>
+#include <multiboot.h>
 #include <Thread.h>
 #include <devices/IO.h>
 #include <devices/Serial.h>
 #include <devices/CPU.h>
+#include <devices/Interrupts.h>
+#include <memory/Paging.h>
+#include <memory/Heap.h>
+#include <filesystem/FileSystem.h>
 #include <string.h>
+#include <Lock.h>
 
-char foo_stack[500];
-void foo(){
-	for(int i = 0; i < 10; i++){
-		com1().write_string("foo()\n");
-		Thread::yield();
-	}
-	Thread::die();
-}
-
-extern "C" int kernel_main(){
-	com1().write_string("greetings\n");
+extern "C" int kernel_main(multiboot_info_t* info){
+	initialize_gdt_table();
+	initialize_interrupts();
+	initialize_paging();
+	com1() << "greetings\n";
 	Thread::initialize();
-	Thread::yield();
-	Thread t((uintptr_t)&foo_stack[499], (uintptr_t)foo);
-	for(int i = 0; i < 20; i++){
-		com1().write_string("kernel_thread\n");
-		Thread::yield();
-	}
 
+	//load the initramfs module into the filesystem
+	multiboot_module_t *mods = (multiboot_module_t*)info->mods_addr;
+	uint32_t *file_header = (uint32_t*)mods->mod_start;
+
+	initialize_file_system(file_header);
+
+	Thread userspace_thread(root_directory().lookup_file("vfs/test.prog"));
+	Thread userspace2(root_directory().lookup_file("vfs/test.prog"));
+	
+	sti();
+	while(true){
+		asm("hlt");
+	}
 	return 0;
 }
