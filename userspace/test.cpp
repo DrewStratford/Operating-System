69e0ba42995c57883c61e8b4ad8e8242108e11d8
Drew Stratford <drewstratford@outlook.com>
2020-07-12 11:47:48 +1200

    Userspace: test now uses FileStream.
    We now write to to stdout using the FileStream class.

--- userspace/test.cpp
+++ userspace/test.cpp
@@ -1,16 +1,17 @@
 #include <System.h>
 #include <string.h>
+#include <FileStream.h>
 
 int main(void){
 	debug("hello from userspace\n");	
 	int stdfd = 0;
+	FileStream stream(stdfd);
 	char buf[61];
 	int c = 0;
 
 	while(c = read(stdfd, buf, 0, 60)){
 		buf[c] = '\0';
-		write(stdfd, (void*)"-->: ", 0, strlen("-->: "));
-		write(stdfd, buf, 0, strlen(buf));
+		stream << "-->: " << buf;
 	}
 	debug("exiting test\n");
 	exit();
