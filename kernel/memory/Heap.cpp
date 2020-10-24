864036f00a7f344184c228bb5a0c3995eeaf1b6c
Drew Stratford <drewstratford@outlook.com>
2020-10-24 17:47:18 +1300

    Memory: add basic freelist malloc.
    The kernel can now allocate memory using a free list
    based system, allowing us to actually free memory.
    
    At current, the implementation is incredibly basic,
    requiring a O(n) scan for both allocation and deletion;
    also, we don't yet do any defragmentation.

--- kernel/memory/Heap.cpp
+++ kernel/memory/Heap.cpp
@@ -1,28 +1,57 @@
 #include <memory/Heap.h>
+#include <devices/Serial.h>
+#include <devices/CPU.h>
 
-static uintptr_t water_mark = 0;
-static uintptr_t water_mark_end = 0;
+FreeNode* free_nodes = nullptr;
+FreeNode end_node = FreeNode(0);
+FreeNode start_node = FreeNode(0);
 
 void initialize_heap(uintptr_t start, uintptr_t end){
-	water_mark = start;
-	water_mark_end = end;
+
+	size_t heap_size = end - start - sizeof(FreeNode);
+	FreeNode* allocation = (FreeNode*)start;
+	*allocation = FreeNode(heap_size);
+
+	start_node.m_next = allocation;
+	allocation->m_previous = &start_node;
+
+	allocation->m_next = &end_node;
+	end_node.m_previous = allocation;
+
+	free_nodes = &start_node;
 }
 
 
 void* kmalloc(size_t size){
-	void* out = (void*)water_mark;
-	water_mark += size;
-	return out;
+	if(size < sizeof(FreeNode))
+		size = sizeof(FreeNode);
+
+	for(FreeNode* node = free_nodes; node != nullptr; node = node->m_next){
+		if(node->split(size)){
+			node->remove();
+
+			if(node->magic == 0xbeef)
+				panic("already 0xbeef");
+
+			node->magic = 0xbeef;
+			return (void*)node->start();
+		}
+	}
+	return nullptr;
 }
 
 void* krealloc(void* ptr, size_t size){
 	if(ptr == nullptr)
 		return kmalloc(size);
 
+	FreeNode* ptr_info = (FreeNode*)(ptr - sizeof(FreeNode));
+
 	uint8_t* new_ptr = (uint8_t*)kmalloc(size);
 	uint8_t* old_ptr = (uint8_t*)ptr;
-	for(int i = 0; i < size; i++)
+	for(int i = 0; i < ptr_info->m_size; i++)
 		new_ptr[i] = old_ptr[i];
+
+	kfree(old_ptr);
 	return new_ptr;
 }
 
@@ -31,13 +60,46 @@ void* kreallocarray(void* ptr, size_t members, size_t size){
 }
 
 void* kmemalign(size_t alignment, size_t size){
-	uintptr_t offset = alignment - (water_mark % alignment);
-	water_mark += offset;
-	return kmalloc(size);
+	for(FreeNode* node = free_nodes; node != nullptr; node = node->m_next){
+		if(node->split_align(size, alignment)){
+			node->remove();
+			com1() << "kmemalign: " << (void*) node->start() << ", " << (void*)node->end() << ", " << (void*)node->magic << "\n";
+			if(node->magic == 0xbeef){
+				panic("already 0xbeef");
+			}
+			node->magic = 0xbeef;
+			return (void*)node->start();
+		}
+	}
+	return nullptr;
 }
 
 void kfree(void* ptr){
-	// Do nothing
+	FreeNode* free = (FreeNode*)(ptr - sizeof(FreeNode));
+
+	if(free->magic == 0xfeed)
+		panic("kfree: freeing free memory!");
+
+	if(free->magic != 0xbeef){
+		com1() << "kfree: trying to free unmanaged memory!\n";
+		return;
+	}
+
+	// Reinsert the node in the free list
+	for(FreeNode* next = free_nodes; next != nullptr; next = next->m_next){
+		if(free->start() < next->start()){
+			auto tmp = next->m_previous;
+			free->m_next = next;
+			free->m_previous = tmp;
+
+			next->m_previous = free;
+			tmp->m_next = free;
+			free->magic = 0xfeed;
+			return;
+		}
+	}
+	com1() << "couldn't free " << ptr << "\n";
+	panic("");
 }
 
 void* operator new(size_t size){
@@ -55,6 +117,7 @@ void operator delete(void* ptr){
 void operator delete(void* ptr, size_t size){
 	kfree(ptr);
 }
+
 
 void operator delete[](void* ptr, size_t size){
 	kfree(ptr);
--- kernel/memory/Heap.h
+++ kernel/memory/Heap.h
@@ -3,6 +3,83 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#include <data/List.h>
+
+class [[gnu::packed]] FreeNode{
+
+public:
+	int32_t magic { 0xfeed };
+	size_t m_size { 0 };
+	FreeNode* m_next { nullptr };
+	FreeNode* m_previous { nullptr };
+
+	FreeNode(size_t size) : m_size(size){
+	}
+
+	size_t start(){
+		return (size_t)this + sizeof(FreeNode);
+	}
+
+	size_t end(){
+		return start() + m_size;
+	}
+
+	bool split(size_t size){
+		if(size == m_size)
+			return true;
+
+		// Can't allocate in this node.
+		if(m_size < size + sizeof(FreeNode))
+			return false;
+
+		// Create a new node at the split point
+		FreeNode* new_node = (FreeNode*)(start() + size);
+		*new_node = FreeNode(m_size - size - sizeof(FreeNode));
+
+		auto* tmp = m_next;
+		this->m_next = new_node;
+		new_node->m_previous = this;
+		new_node->m_next = tmp;
+		tmp->m_previous = new_node;
+
+		m_size = size;
+		return true;
+	}
+
+	bool split_align(size_t size, size_t alignment){
+		// We're aligned so can malloc
+		if(start() % alignment == 0)
+			return split(size);
+
+		// TODO: this is hacky
+		// Attempt to create a new split that has the proper alignment.
+		// This split will always fail, but when malloc checks the next
+		// node it should succeed.
+
+		intptr_t next_alignment = (intptr_t)alignment - ((intptr_t)start() % (intptr_t)alignment);
+		//intptr_t split_size = next_alignment - (intptr_t)sizeof(size_t) - (intptr_t)sizeof(int32_t);
+		intptr_t split_size = next_alignment - (intptr_t)sizeof(FreeNode);
+
+		// Can't suitably split this node.
+		if(split_size < 0)
+			return false;
+
+		// This should create a new node with the right alignment.
+		// However, we don't now whether it will actually fit!
+		split((size_t)split_size);
+		return false;
+	}
+
+	void remove(){
+		if(m_next == nullptr || m_previous == nullptr)
+			return;
+
+		m_previous->m_next = m_next;
+		m_next->m_previous = m_previous;
+		m_previous = nullptr;
+		m_next = nullptr;
+	}
+};
 
 void initialize_heap(uintptr_t start, uintptr_t end);
 
