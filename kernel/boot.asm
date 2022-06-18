a36683782244142d9c5a232523ffc9a53aa958c6
Drew Stratford <drewstratford@outlook.com>
2022-06-18 23:09:50 +1200

    Boot: enable SSE before calling constructors.
    It looks like the new version of GCC emits
    SSE instructors more liberally.
    
    To avoid crashing when encountering any of these
    instructions, we need to enable SSE at boot.

--- kernel/boot.asm
+++ kernel/boot.asm
@@ -34,8 +34,22 @@ _start:
 	; set up stack
 	mov esp, stack_top
  
-	;
 	push ebx		;push the GRUB multiboot info
+
+	; Check for sse instructions
+	mov eax, 0x1
+	cpuid
+	test edx, 1<<25
+	jz .hang
+
+	; We have sse so enable it
+	mov eax, cr0
+	and ax, 0xFFFB		;clear coprocessor emulation CR0.EM
+	or ax, 0x2			;set coprocessor monitoring  CR0.MP
+	mov cr0, eax
+	mov eax, cr4
+	or ax, 3 << 9		;set CR4.OSFXSR and CR4.OSXMMEXCPT at the same time
+	mov cr4, eax
 
 	extern __CTOR_LIST__
 	extern __CTOR_END__
